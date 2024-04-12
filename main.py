fa6214ca7ff5a67ccfd43e646be4cd3c76406050
AlRogov <166138869+AlRogov@users.noreply.github.com>
2024-04-12 17:48:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+name = 'это моя строка'
+print(name[0])
+
+name = 'это моя строка'
+print(name[-1])
+
+name = 'это моя строка'
+print(name[1:3])
+
+name = 'это моя строка'
+print(name[::-1])
+
+name = 'это моя строка'
+print(len(name))
+
+name = 'это моя строка'
+print('это новая строка ' + name)
